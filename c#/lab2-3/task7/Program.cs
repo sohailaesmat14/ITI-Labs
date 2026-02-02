4634debc8b2dd3d53f39d9daf7988c11ee227953
Sohaila Esmat <sohailabs1410@gmail.com>
2026-02-02 17:56:24 +0200

    Add files via upload

---
+++ c#/lab2-3/task7/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+public static class ArrayUtils
+{
+    public static void Reverse(int[] arr)
+    {
+        int start = 0;
+        int end = arr.Length - 1;
+        while (start < end)
+        {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static int FindMax(int[] arr)
+    {
+        int max = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] > max) max = arr[i];
+        }
+        return max;
+    }
+
+    public static int FindMin(int[] arr)
+    {
+        int min = arr[0];
+        for (int i = 1; i < arr.Length; i++)
+        {
+            if (arr[i] < min) min = arr[i];
+        }
+        return min;
+    }
+
+    public static bool IsSorted(int[] arr)
+    {
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] > arr[i + 1]) return false;
+        }
+        return true;
+    }
+
+    public static int CountOccurrences(int[] arr, int value)
+    {
+        int count = 0;
+        foreach (int item in arr)
+        {
+            if (item == value) count++;
+        }
+        return count;
+    }
+
+    public static int[] Merge(int[] arr1, int[] arr2)
+    {
+        int[] result = new int[arr1.Length + arr2.Length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < arr1.Length && j < arr2.Length)
+        {
+            if (arr1[i] < arr2[j]) result[k++] = arr1[i++];
+            else result[k++] = arr2[j++];
+        }
+
+        while (i < arr1.Length) result[k++] = arr1[i++];
+        while (j < arr2.Length) result[k++] = arr2[j++];
+
+        return result;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numbers = { 5, 2, 8, 1, 9 };
+
+        Console.WriteLine("Max: " + ArrayUtils.FindMax(numbers));
+        Console.WriteLine("Min: " + ArrayUtils.FindMin(numbers));
+        Console.WriteLine("Is Sorted: " + ArrayUtils.IsSorted(numbers));
+
+        ArrayUtils.Reverse(numbers);
+        Console.WriteLine("Reversed: " + string.Join(", ", numbers));
+
+        int[] a = { 1, 3, 5 };
+        int[] b = { 2, 4, 6 };
+        int[] merged = ArrayUtils.Merge(a, b);
+        Console.WriteLine("Merged: " + string.Join(", ", merged));
+
+        Console.ReadKey();
+    }
+}
---
+++ c#/lab2-3/task8/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        string input = "The cat and the dog and the bird";
+        
+        string lowerInput = input.ToLower();
+        string[] words = lowerInput.Split(' ');
+
+        string[] uniqueWords = new string[words.Length];
+        int[] counts = new int[words.Length];
+        int uniqueCount = 0;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            string currentWord = words[i];
+            bool found = false;
+
+            for (int j = 0; j < uniqueCount; j++)
+            {
+                if (string.Compare(uniqueWords[j], currentWord) == 0)
+                {
+                    counts[j]++;
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                uniqueWords[uniqueCount] = currentWord;
+                counts[uniqueCount] = 1;
+                uniqueCount++;
+            }
+        }
+
+        for (int i = 0; i < uniqueCount - 1; i++)
+        {
+            for (int j = 0; j < uniqueCount - i - 1; j++)
+            {
+                if (counts[j] < counts[j + 1])
+                {
+                    int tempCount = counts[j];
+                    counts[j] = counts[j + 1];
+                    counts[j + 1] = tempCount;
+
+                    string tempWord = uniqueWords[j];
+                    uniqueWords[j] = uniqueWords[j + 1];
+                    uniqueWords[j + 1] = tempWord;
+                }
+            }
+        }
+
+        for (int i = 0; i < uniqueCount; i++)
+        {
+            string result = string.Concat(uniqueWords[i], ": ", counts[i]);
+            Console.WriteLine(result);
+        }
+        Console.ReadKey();
+    }
+}
