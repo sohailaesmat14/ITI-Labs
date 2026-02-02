e4a87952e4e85a4a67e4039c5da4a2f2bc56cb25
Sohaila Esmat <sohailabs1410@gmail.com>
2026-02-02 17:53:18 +0200

    Add files via upload

---
+++ c#/lab2-3/task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int n = 4;
+        int[,] matrix = GenerateSpiralMatrix(n);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matrix[i, j].ToString().PadLeft(4));
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+
+    public static int[,] GenerateSpiralMatrix(int n)
+    {
+        int[,] result = new int[n, n];
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+        int num = 1;
+
+        while (num <= n * n)
+        {
+            for (int i = left; i <= right; i++)
+                result[top, i] = num++;
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                result[i, right] = num++;
+            right--;
+
+            for (int i = right; i >= left; i--)
+                result[bottom, i] = num++;
+            bottom--;
+
+            for (int i = bottom; i >= top; i--)
+                result[i, left] = num++;
+            left++;
+        }
+
+        return result;
+    }
+}
