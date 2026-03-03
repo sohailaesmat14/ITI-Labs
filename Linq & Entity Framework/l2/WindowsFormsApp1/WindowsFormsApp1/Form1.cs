28b085d4b1ad72eb9f39494243c0b21d8a82828d
Sohaila Esmat <sohailabs1410@gmail.com>
2026-03-03 06:50:15 +0200

    Add files via upload

---
+++ Linq & Entity Framework/l2/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=lab1;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Employees";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    conn.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        dgvEmployees.DataSource = dt;
+                    }
+                }
+            }
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "INSERT INTO Employees (Id, Name, Department) VALUES (@id, @name, @dept)";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txtId.Text));
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@dept", txtDepartment.Text);
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Employee added successfully");
+                }
+            }
+            btnDisplay_Click(null, null);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "UPDATE Employees SET Name = @name, Department = @dept WHERE Id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txtId.Text));
+                    cmd.Parameters.AddWithValue("@name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@dept", txtDepartment.Text);
+
+                    conn.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                        MessageBox.Show("Employee data updated successfully");
+                    else
+                        MessageBox.Show("Employee not found to update");
+                }
+            }
+            btnDisplay_Click(null, null);
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT Name, Department FROM Employees WHERE Id = @id";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@id", int.Parse(txtId.Text));
+
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            txtName.Text = reader["Name"].ToString();
+                            txtDepartment.Text = reader["Department"].ToString();
+                            MessageBox.Show("Employee found");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Employee not found");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
